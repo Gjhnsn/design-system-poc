d0d02dc79848842951de82cef84231817ab2dfef
Garrett Johnson <garrettjtrains@gmail.com>
2025-01-26 11:44:33 -0800

    update config

--- modern.config.ts
+++ modern.config.ts
@@ -9,6 +9,14 @@ export default defineConfig({
   runtime: {
     router: true,
   },
+  output: {
+    distPath: {
+      root: './dist', // Ensures build files are output to the `dist` folder
+    },
+    assetPrefix: process.env.NODE_ENV === 'production'
+      ? 'https://design-system-poc-ten.vercel.app/' // Your production URL
+      : 'http://localhost:3001/', // Local development URL
+  },
   plugins: [
     appTools({
       bundler: 'rspack', // Set to 'webpack' to enable webpack
